daa48e78ee90f26a85d83acc685553305cf66445
Bernardo Lustrini <blustrini@gmail.com>
2024-05-14 10:04:03 +0100

    fix __init__...!?

--- src/bnn/__init__.py
+++ src/bnn/__init__.py
@@ -1,3 +1,10 @@
 """Top-level package for bnn."""
 
-from . import *
+__all__ = [
+    'functions',  # noqa: F405
+    'layer',  # noqa: F405
+    'network',  # noqa: F405
+    'random',  # noqa: F405
+]
+
+from bnn import *
