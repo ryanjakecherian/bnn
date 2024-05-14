b69900336896777554e83a17d99969fa68953a54
Bernardo Lustrini <blustrini@gmail.com>
2024-05-14 10:22:35 +0100

    remove star import

--- src/bnn/__init__.py
+++ src/bnn/__init__.py
@@ -2,11 +2,9 @@
 
 # HACK how is this done?
 __all__ = [
-    'functions',  # noqa: F405
-    'layer',  # noqa: F405
-    'network',  # noqa: F405
-    'random',  # noqa: F405
-    'loss',  # noqa: F405
+    'functions',
+    'layer',
+    'network',
+    'random',
+    'loss',
 ]
-
-from bnn import *
