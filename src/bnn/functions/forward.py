5670702add206836146668c653a2de2e96033ffa
Bernardo Lustrini <blustrini@gmail.com>
2024-05-24 11:26:03 +0100

    implemented layer mean binarisation and layer median binarisation

--- src/bnn/functions/forward.py
+++ src/bnn/functions/forward.py
@@ -10,9 +10,42 @@ class ForwardFunc(abc.ABC):
     def __call__(self, x: torch.Tensor, W: torch.Tensor) -> torch.Tensor: ...
 
 
-class SignBinarise(ForwardFunc):
+class MatMulBinarise(ForwardFunc):
     def __call__(self, x: torch.Tensor, W: torch.Tensor) -> torch.Tensor:
         integer = x @ W
-        out_binary = functions.binarise(x=integer, threshold=0)
+        out_binary = self.binarise(x=integer)
 
         return out_binary
+
+    @abc.abstractmethod
+    def binarise(self, x: torch.Tensor) -> torch.Tensor: ...
+
+
+class SignBinarise(MatMulBinarise):
+    def binarise(self, x: torch.Tensor) -> torch.Tensor:
+        return functions.binarise(x=x, threshold=0)
+
+
+class LayerMeanBinarise(MatMulBinarise):
+    def binarise(self, x: torch.Tensor) -> torch.Tensor:
+        means = torch.mean(x.to(torch.float), dim=-1)
+
+        out = torch.empty_like(x)
+
+        for i, (x_, mean) in enumerate(zip(x, means)):
+            out[i] = functions.binarise(x=x_, threshold=mean)
+
+        return out
+
+
+class LayerMedianBinarise(MatMulBinarise):
+    def binarise(self, x: torch.Tensor) -> torch.Tensor:
+        medians_out = torch.median(x, dim=-1)
+        medians = medians_out.values
+
+        out = torch.empty_like(x)
+
+        for i, (x_, median) in enumerate(zip(x, medians)):
+            out[i] = functions.binarise(x=x_, threshold=median)
+
+        return out
