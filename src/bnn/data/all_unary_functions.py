2dc0c18c6527c56beeb31b7e2fd191c57c144786
Bernardo Lustrini <blustrini@gmail.com>
2024-05-27 18:35:37 +0100

    generate unary functions on the fly

--- src/bnn/data/all_unary_functions.py
+++ src/bnn/data/all_unary_functions.py
@@ -7,7 +7,6 @@ from .data_loader import DataLoader, LabelledDatum
 
 class AllUnaryFunctionsDataLoader(DataLoader):
     _input_dim: int
-    _datum: LabelledDatum
 
     def __init__(
         self,
@@ -22,22 +21,15 @@ class AllUnaryFunctionsDataLoader(DataLoader):
             batch_size=batch_size,
             include_last_if_uneven=include_last_if_uneven,
         )
-        self._generate_datum()
-
-    def _generate_datum(self):
-        input = bnn.functions.binarise(torch.randn(self._datapoints, self._input_dim)).to(torch.int)
-        ones_like_input = torch.ones_like(input)
-        target = torch.concat(
-            tensors=[input, -input, ones_like_input, -ones_like_input],
-            dim=-1,
-        )
-
-        self._datum = LabelledDatum(input=input, target=target)
 
     def _healthcheck(self):
         pass
 
     def _next(self, size: int) -> LabelledDatum:
-        input = self._datum.input[:size]
-        target = self._datum.target[:size]
+        input = bnn.functions.binarise(torch.randn(size, self._input_dim)).to(torch.int)
+        ones_like_input = torch.ones_like(input)
+        target = torch.concat(
+            tensors=[input, -input, ones_like_input, -ones_like_input],
+            dim=-1,
+        )
         return LabelledDatum(input=input, target=target)
