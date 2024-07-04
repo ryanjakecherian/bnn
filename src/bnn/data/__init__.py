72db19177081d0baa66880df3608d9c71523e148
Bernardo Lustrini <blustrini@gmail.com>
2024-07-04 11:25:40 +0100

    fashion mnist...!

--- src/bnn/data/__init__.py
+++ src/bnn/data/__init__.py
@@ -4,9 +4,11 @@ __all__ = [
     'TargetNetworkDataLoader',
     'AllUnaryFunctionsDataLoader',
     'MNISTDataLoader',
+    'FashionMNISTDataLoader',
 ]
 
 from .all_unary_functions import AllUnaryFunctionsDataLoader
 from .data_loader import DataLoader, LabelledDatum
+from .fashion_mnist import FashionMNISTDataLoader
 from .mnist import MNISTDataLoader
 from .target_network import TargetNetworkDataLoader
